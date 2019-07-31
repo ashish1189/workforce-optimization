3759374cf06cad9e5095500bb771657055661710
Ashish Deshpande <ashishdeshpande123@gmail.com>
2019-07-31 23:40:01 +0530

    Draft | Initial Commit | Initializing assignment project

---
+++ src/main/java/com/spo/cleaners/optimization/WorkforceOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.spo.cleaners.optimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforceOptimizationApplication.class, args);
+	}
+
+}
